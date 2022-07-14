b104df637e559f95ab695e719f6ec93c33aa7d97
David Ils <david_ils@web.de>
2022-07-14 11:42:16 +0200

    added main functionality

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+// Get all "Show modal" buttons
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// Add EventListener to each "Show modal" button
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+// Add EventListener to "close" button
+btnCloseModal.addEventListener('click', closeModal);
+
+// Add EventListener to "overlay"
+overlay.addEventListener('click', closeModal);
+
+// Add EventListener when "Escape" key is pressed
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
